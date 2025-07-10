fe4e4d16001d3d0fe824696112f23e4d46a71792
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2025-07-10 20:07:09 +0900

    ui: 실시간 success, error 판단하는 input field

--- lib/design_system/component/app_input_field.dart
+++ lib/design_system/component/app_input_field.dart
@@ -6,59 +6,18 @@ import 'package:gllo_flutter/design_system/foundation/size/app_layout.dart';
 
 enum InputStatus { defaultStatus, success, error, disabled }
 
-class AppInputField extends FormField<String> {
-  AppInputField({
+class AppInputField extends StatefulWidget {
+  const AppInputField({
     super.key,
-    super.validator,
     required this.label,
     required this.controller,
     required this.placeholder,
     this.isPassword = false,
     this.isEnabled = true,
     this.helpMessage,
+    this.errorText,
     this.suffixIcon,
-    ValueChanged<String>? onChanged,
-  }) : super(
-         initialValue: controller.text,
-         enabled: isEnabled,
-         builder: (FormFieldState<String> field) {
-           return _AppInputFieldInternal(
-             label: label,
-             controller: controller,
-             placeholder: placeholder,
-             isPassword: isPassword,
-             isEnabled: isEnabled,
-             errorText: field.errorText,
-             helpMessage: helpMessage,
-             suffixIcon: suffixIcon,
-             onChanged: (value) {
-               field.didChange(value);
-               onChanged?.call(value);
-             },
-           );
-         },
-       );
-
-  final String label;
-  final TextEditingController controller;
-  final String placeholder;
-  final bool isPassword;
-  final bool isEnabled;
-  final String? helpMessage;
-  final Widget? suffixIcon;
-}
-
-class _AppInputFieldInternal extends StatefulWidget {
-  const _AppInputFieldInternal({
-    required this.label,
-    required this.controller,
-    required this.placeholder,
-    required this.isPassword,
-    required this.isEnabled,
-    required this.onChanged,
-    required this.errorText,
-    this.helpMessage,
-    this.suffixIcon,
+    this.onChanged,
   });
 
   final String label;
@@ -68,32 +27,38 @@ class _AppInputFieldInternal extends StatefulWidget {
   final bool isEnabled;
   final String? helpMessage;
   final String? errorText;
-  final ValueChanged<String> onChanged;
   final Widget? suffixIcon;
+  final ValueChanged<String>? onChanged;
 
   @override
-  State<_AppInputFieldInternal> createState() =>
-      _AppFormInputFieldInternalState();
+  State<AppInputField> createState() => _AppInputFieldState();
 }
 
-class _AppFormInputFieldInternalState extends State<_AppInputFieldInternal> {
-  bool isObscured = true;
+class _AppInputFieldState extends State<AppInputField> {
+  final ValueNotifier<bool> isObscured = ValueNotifier<bool>(true);
+  final FocusNode _focusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode.addListener(() => setState(() {})); // 포커스 변경 감지
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    isObscured.dispose();
+    super.dispose();
+  }
 
   InputStatus getStatus() {
     if (!widget.isEnabled) return InputStatus.disabled;
     if (widget.errorText != null) return InputStatus.error;
-    if (widget.errorText == null && widget.controller.text.isNotEmpty) {
-      return InputStatus.success;
-    }
+    if (widget.controller.text.isNotEmpty) return InputStatus.success;
     return InputStatus.defaultStatus;
   }
 
-  OutlineInputBorder border(Color color) => OutlineInputBorder(
-    borderRadius: BorderRadius.circular(AppLayout.radius300),
-    borderSide: BorderSide(color: color, width: AppLayout.stroke15),
-  );
-
-  Color getBorderColor(InputStatus status) {
+  Color _getBorderColor(InputStatus status) {
     switch (status) {
       case InputStatus.disabled:
         return AppScaleColor.gray500;
@@ -109,48 +74,15 @@ class _AppFormInputFieldInternalState extends State<_AppInputFieldInternal> {
   @override
   Widget build(BuildContext context) {
     final status = getStatus();
-    final isError = status == InputStatus.error;
-    final isDisabled = status == InputStatus.disabled;
+    final isFocused = _focusNode.hasFocus;
 
-    final suffixIcon =
-        widget.isPassword
-            ? GestureDetector(
-              onTap: () => setState(() => isObscured = !isObscured),
-              child: Padding(
-                padding: const EdgeInsets.all(AppLayout.marginPaddingS),
-                child:
-                    isObscured
-                        ? Assets.icon.system.eyeCloseLine.svg(
-                          width: 24,
-                          height: 24,
-                          colorFilter: const ColorFilter.mode(
-                            AppScaleColor.gray500,
-                            BlendMode.srcIn,
-                          ),
-                        )
-                        : Assets.icon.system.eye2Line.svg(
-                          width: 24,
-                          height: 24,
-                          colorFilter: const ColorFilter.mode(
-                            AppScaleColor.gray500,
-                            BlendMode.srcIn,
-                          ),
-                        ),
-              ),
-            )
-            : status == InputStatus.success
-            ? Padding(
-              padding: const EdgeInsets.all(AppLayout.marginPaddingS),
-              child: Assets.icon.system.checkCircleFill.svg(
-                width: 24,
-                height: 24,
-                colorFilter: const ColorFilter.mode(
-                  AppScaleColor.gray700,
-                  BlendMode.srcIn,
-                ),
-              ),
-            )
-            : widget.suffixIcon;
+    final border = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(AppLayout.radius300),
+      borderSide: BorderSide(
+        color: _getBorderColor(status),
+        width: AppLayout.stroke15,
+      ),
+    );
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -162,57 +94,146 @@ class _AppFormInputFieldInternalState extends State<_AppInputFieldInternal> {
           child: Text(
             widget.label,
             style: AppTextStyle.textMm.copyWith(
-              color: isError ? AppScaleColor.red : AppScaleColor.gray800,
+              color:
+                  status == InputStatus.error
+                      ? AppScaleColor.red
+                      : AppScaleColor.gray800,
             ),
           ),
         ),
         const SizedBox(height: AppLayout.marginPaddingXxs),
         SizedBox(
           height: 48,
-          child: TextField(
-            controller: widget.controller,
-            enabled: widget.isEnabled,
-            obscureText: widget.isPassword && isObscured,
-            obscuringCharacter: '●',
-            style: AppTextStyle.textMm.copyWith(color: AppScaleColor.gray800),
-            onChanged: widget.onChanged,
-            decoration: InputDecoration(
-              filled: true,
-              fillColor:
-                  isDisabled ? Colors.grey.shade100 : AppScaleColor.gray50,
-              hintText: widget.placeholder,
-              hintStyle: AppTextStyle.textMm.copyWith(
-                color: AppScaleColor.gray400,
-              ),
-              contentPadding: const EdgeInsets.symmetric(
-                horizontal: AppLayout.marginPaddingM,
-                vertical: 0,
-              ),
-              border: border(getBorderColor(status)),
-              enabledBorder: border(getBorderColor(status)),
-              focusedBorder: border(getBorderColor(status)),
-              disabledBorder: border(getBorderColor(status)),
-              errorBorder: border(AppScaleColor.red),
-              suffixIcon: suffixIcon,
-            ),
+          child: ValueListenableBuilder<bool>(
+            valueListenable: isObscured,
+            builder: (context, obscure, _) {
+              return TextField(
+                controller: widget.controller,
+                focusNode: _focusNode,
+                enabled: widget.isEnabled,
+                obscureText: widget.isPassword && obscure,
+                obscuringCharacter: '●',
+                onChanged: (value) {
+                  widget.onChanged?.call(value);
+                  setState(() {}); // 아이콘 갱신
+                },
+                style: AppTextStyle.textMm.copyWith(
+                  color:
+                      status == InputStatus.error
+                          ? AppScaleColor.red
+                          : AppScaleColor.gray800,
+                ),
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor:
+                      status == InputStatus.disabled
+                          ? Colors.grey.shade100
+                          : AppScaleColor.gray50,
+                  hintText: widget.placeholder,
+                  hintStyle: AppTextStyle.textMm.copyWith(
+                    color: AppScaleColor.gray400,
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(
+                    horizontal: AppLayout.marginPaddingM,
+                    vertical: 0,
+                  ),
+                  border: border,
+                  enabledBorder: border,
+                  focusedBorder: border,
+                  disabledBorder: border,
+                  suffixIcon: _buildSuffixIcon(status, isFocused),
+                ),
+              );
+            },
           ),
         ),
         if (widget.errorText != null || widget.helpMessage != null)
           Padding(
             padding: const EdgeInsets.fromLTRB(
-              AppLayout.marginPaddingXs, // left
-              AppLayout.marginPaddingXxs, // top
-              AppLayout.marginPaddingXs, // right
-              0, // bottom
+              AppLayout.marginPaddingXs,
+              AppLayout.marginPaddingXxs,
+              AppLayout.marginPaddingXs,
+              0,
             ),
             child: Text(
               widget.errorText ?? widget.helpMessage!,
               style: AppTextStyle.textSr.copyWith(
-                color: isError ? AppScaleColor.red : AppScaleColor.gray500,
+                color:
+                    status == InputStatus.error
+                        ? AppScaleColor.red
+                        : AppScaleColor.gray500,
               ),
             ),
           ),
       ],
     );
+  }
+
+  Widget? _buildSuffixIcon(InputStatus status, bool isFocused) {
+    final hasText = widget.controller.text.isNotEmpty;
+
+    if (widget.isPassword) {
+      return GestureDetector(
+        onTap: () => isObscured.value = !isObscured.value,
+        child: Padding(
+          padding: const EdgeInsets.all(AppLayout.marginPaddingS),
+          child:
+              isObscured.value
+                  ? Assets.icon.system.eyeCloseLine.svg(
+                    width: 24,
+                    height: 24,
+                    colorFilter: const ColorFilter.mode(
+                      AppScaleColor.gray500,
+                      BlendMode.srcIn,
+                    ),
+                  )
+                  : Assets.icon.system.eye2Line.svg(
+                    width: 24,
+                    height: 24,
+                    colorFilter: const ColorFilter.mode(
+                      AppScaleColor.gray500,
+                      BlendMode.srcIn,
+                    ),
+                  ),
+        ),
+      );
+    }
+
+    if (status == InputStatus.success) {
+      return Padding(
+        padding: const EdgeInsets.all(AppLayout.marginPaddingS),
+        child: Assets.icon.system.checkCircleFill.svg(
+          width: 24,
+          height: 24,
+          colorFilter: const ColorFilter.mode(
+            AppScaleColor.gray700,
+            BlendMode.srcIn,
+          ),
+        ),
+      );
+    }
+
+    if (hasText && isFocused) {
+      return GestureDetector(
+        onTap: () {
+          widget.controller.clear();
+          widget.onChanged?.call('');
+          setState(() {});
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(AppLayout.marginPaddingS),
+          child: Assets.icon.system.cancel.svg(
+            width: 24,
+            height: 24,
+            colorFilter: const ColorFilter.mode(
+              AppScaleColor.gray500,
+              BlendMode.srcIn,
+            ),
+          ),
+        ),
+      );
+    }
+
+    return widget.suffixIcon;
   }
 }
