e558fd5d02124cd7727f87ec5fd6cd20b02312cf
generalChang <rhdrnsckdgh0921@naver.com>
2025-07-13 23:08:04 +0900

    feat: 로그인 및 회원가입 탭 텍스트 데이터 추가

--- lib/app/localization/locale_keys.g.dart
+++ lib/app/localization/locale_keys.g.dart
@@ -12,4 +12,178 @@ abstract class LocaleKeys {
   static const onboarding_desc3 = 'onboarding.desc3';
   static const onboarding_nextButton = 'onboarding.nextButton';
   static const onboarding = 'onboarding';
+  static const startSignUp_title = 'startSignUp.title';
+  static const startSignUp_signUpButtonText = 'startSignUp.signUpButtonText';
+  static const startSignUp_signInButtonText = 'startSignUp.signInButtonText';
+  static const startSignUp_findAccount = 'startSignUp.findAccount';
+  static const startSignUp_socialSignInDesc = 'startSignUp.socialSignInDesc';
+  static const startSignUp = 'startSignUp';
+  static const signIn_appBarTitle = 'signIn.appBarTitle';
+  static const signIn_idHintText = 'signIn.idHintText';
+  static const signIn_passwordHintText = 'signIn.passwordHintText';
+  static const signIn_signInButtonText = 'signIn.signInButtonText';
+  static const signIn_findAccount = 'signIn.findAccount';
+  static const signIn_socialSignInDesc = 'signIn.socialSignInDesc';
+  static const signIn_error_notFoundId = 'signIn.error.notFoundId';
+  static const signIn_error_wrongPassword = 'signIn.error.wrongPassword';
+  static const signIn_error = 'signIn.error';
+  static const signIn = 'signIn';
+  static const signUpBirthDay_appBarTitle = 'signUpBirthDay.appBarTitle';
+  static const signUpBirthDay_title = 'signUpBirthDay.title';
+  static const signUpBirthDay_desc = 'signUpBirthDay.desc';
+  static const signUpBirthDay_inputHintText = 'signUpBirthDay.inputHintText';
+  static const signUpBirthDay_nextButtonText = 'signUpBirthDay.nextButtonText';
+  static const signUpBirthDay_error_invalidBirthDay =
+      'signUpBirthDay.error.invalidBirthDay';
+  static const signUpBirthDay_error = 'signUpBirthDay.error';
+  static const signUpBirthDay = 'signUpBirthDay';
+  static const signUpTerms_appBarTitle = 'signUpTerms.appBarTitle';
+  static const signUpTerms_title = 'signUpTerms.title';
+  static const signUpTerms_desc = 'signUpTerms.desc';
+  static const signUpTerms_allAgreeText = 'signUpTerms.allAgreeText';
+  static const signUpTerms_termsText = 'signUpTerms.termsText';
+  static const signUpTerms_locationBasedTermsText =
+      'signUpTerms.locationBasedTermsText';
+  static const signUpTerms_marketingConsentText =
+      'signUpTerms.marketingConsentText';
+  static const signUpTerms_label_required = 'signUpTerms.label.required';
+  static const signUpTerms_label_optional = 'signUpTerms.label.optional';
+  static const signUpTerms_label = 'signUpTerms.label';
+  static const signUpTerms_nextButtonText = 'signUpTerms.nextButtonText';
+  static const signUpTerms = 'signUpTerms';
+  static const signUpEmail_appBarTitle = 'signUpEmail.appBarTitle';
+  static const signUpEmail_title = 'signUpEmail.title';
+  static const signUpEmail_desc = 'signUpEmail.desc';
+  static const signUpEmail_emailInputHintText =
+      'signUpEmail.emailInputHintText';
+  static const signUpEmail_verifyStudentIdCard =
+      'signUpEmail.verifyStudentIdCard';
+  static const signUpEmail_codeInputHintText = 'signUpEmail.codeInputHintText';
+  static const signUpEmail_resendCodeText = 'signUpEmail.resendCodeText';
+  static const signUpEmail_sendCodeText = 'signUpEmail.sendCodeText';
+  static const signUpEmail_verifyCodeText = 'signUpEmail.verifyCodeText';
+  static const signUpEmail_emailInputHelperMessage =
+      'signUpEmail.emailInputHelperMessage';
+  static const signUpEmail_nextButtonText = 'signUpEmail.nextButtonText';
+  static const signUpEmail_error_invalidEmail =
+      'signUpEmail.error.invalidEmail';
+  static const signUpEmail_error_wrongCode = 'signUpEmail.error.wrongCode';
+  static const signUpEmail_error = 'signUpEmail.error';
+  static const signUpEmail = 'signUpEmail';
+  static const signUpNameAndPassword_appBarTitle =
+      'signUpNameAndPassword.appBarTitle';
+  static const signUpNameAndPassword_title = 'signUpNameAndPassword.title';
+  static const signUpNameAndPassword_desc = 'signUpNameAndPassword.desc';
+  static const signUpNameAndPassword_nameInputHintText =
+      'signUpNameAndPassword.nameInputHintText';
+  static const signUpNameAndPassword_nameInputHelperMessage =
+      'signUpNameAndPassword.nameInputHelperMessage';
+  static const signUpNameAndPassword_canUseNameHelperMessage =
+      'signUpNameAndPassword.canUseNameHelperMessage';
+  static const signUpNameAndPassword_passwordInputHintText =
+      'signUpNameAndPassword.passwordInputHintText';
+  static const signUpNameAndPassword_passwordInputHelperMessage =
+      'signUpNameAndPassword.passwordInputHelperMessage';
+  static const signUpNameAndPassword_confirmPasswordInputHintText =
+      'signUpNameAndPassword.confirmPasswordInputHintText';
+  static const signUpNameAndPassword_nextButtonText =
+      'signUpNameAndPassword.nextButtonText';
+  static const signUpNameAndPassword_error_duplicateName =
+      'signUpNameAndPassword.error.duplicateName';
+  static const signUpNameAndPassword_error_invalidName =
+      'signUpNameAndPassword.error.invalidName';
+  static const signUpNameAndPassword_error_exceedLimit =
+      'signUpNameAndPassword.error.exceedLimit';
+  static const signUpNameAndPassword_error_mismatchPassword =
+      'signUpNameAndPassword.error.mismatchPassword';
+  static const signUpNameAndPassword_error = 'signUpNameAndPassword.error';
+  static const signUpNameAndPassword = 'signUpNameAndPassword';
+  static const signUpCountry_appBarTitle = 'signUpCountry.appBarTitle';
+  static const signUpCountry_title = 'signUpCountry.title';
+  static const signUpCountry_desc = 'signUpCountry.desc';
+  static const signUpCountry_countryInputHintText =
+      'signUpCountry.countryInputHintText';
+  static const signUpCountry_nextButtonText = 'signUpCountry.nextButtonText';
+  static const signUpCountry = 'signUpCountry';
+  static const signUpSuccess_title = 'signUpSuccess.title';
+  static const signUpSuccess_desc = 'signUpSuccess.desc';
+  static const signUpSuccess_buttonText = 'signUpSuccess.buttonText';
+  static const signUpSuccess = 'signUpSuccess';
+  static const signUpStudentIdCard_appBarTitle =
+      'signUpStudentIdCard.appBarTitle';
+  static const signUpStudentIdCard_title = 'signUpStudentIdCard.title';
+  static const signUpStudentIdCard_desc = 'signUpStudentIdCard.desc';
+  static const signUpStudentIdCard_imageUploadLabel =
+      'signUpStudentIdCard.imageUploadLabel';
+  static const signUpStudentIdCard_imageUploadHintText =
+      'signUpStudentIdCard.imageUploadHintText';
+  static const signUpStudentIdCard_openCameraButtonText =
+      'signUpStudentIdCard.openCameraButtonText';
+  static const signUpStudentIdCard_nextButtonText =
+      'signUpStudentIdCard.nextButtonText';
+  static const signUpStudentIdCard_error_exceedCapacity_title =
+      'signUpStudentIdCard.error.exceedCapacity.title';
+  static const signUpStudentIdCard_error_exceedCapacity_desc =
+      'signUpStudentIdCard.error.exceedCapacity.desc';
+  static const signUpStudentIdCard_error_exceedCapacity_button =
+      'signUpStudentIdCard.error.exceedCapacity.button';
+  static const signUpStudentIdCard_error_exceedCapacity =
+      'signUpStudentIdCard.error.exceedCapacity';
+  static const signUpStudentIdCard_error = 'signUpStudentIdCard.error';
+  static const signUpStudentIdCard = 'signUpStudentIdCard';
+  static const findAccount_appBarTitle = 'findAccount.appBarTitle';
+  static const findAccount_findIdTab_name = 'findAccount.findIdTab.name';
+  static const findAccount_findIdTab_idInputHintText =
+      'findAccount.findIdTab.idInputHintText';
+  static const findAccount_findIdTab_idInputHelperMessage =
+      'findAccount.findIdTab.idInputHelperMessage';
+  static const findAccount_findIdTab_sendCodeText =
+      'findAccount.findIdTab.sendCodeText';
+  static const findAccount_findIdTab_verifyCodeText =
+      'findAccount.findIdTab.verifyCodeText';
+  static const findAccount_findIdTab_codeInputHelperMessage =
+      'findAccount.findIdTab.codeInputHelperMessage';
+  static const findAccount_findIdTab_error_notFoundId =
+      'findAccount.findIdTab.error.notFoundId';
+  static const findAccount_findIdTab_error_wrongCode =
+      'findAccount.findIdTab.error.wrongCode';
+  static const findAccount_findIdTab_error = 'findAccount.findIdTab.error';
+  static const findAccount_findIdTab_success_title =
+      'findAccount.findIdTab.success.title';
+  static const findAccount_findIdTab_success_desc =
+      'findAccount.findIdTab.success.desc';
+  static const findAccount_findIdTab_success_signInButtonText =
+      'findAccount.findIdTab.success.signInButtonText';
+  static const findAccount_findIdTab_success = 'findAccount.findIdTab.success';
+  static const findAccount_findIdTab = 'findAccount.findIdTab';
+  static const findAccount_findPasswordTab_name =
+      'findAccount.findPasswordTab.name';
+  static const findAccount_findPasswordTab_passwordInputLabel =
+      'findAccount.findPasswordTab.passwordInputLabel';
+  static const findAccount_findPasswordTab_passwordInputHintText =
+      'findAccount.findPasswordTab.passwordInputHintText';
+  static const findAccount_findPasswordTab_confirmPasswordInputHintText =
+      'findAccount.findPasswordTab.confirmPasswordInputHintText';
+  static const findAccount_findPasswordTab_passwordHelperMessage =
+      'findAccount.findPasswordTab.passwordHelperMessage';
+  static const findAccount_findPasswordTab_resetPasswordButtonText =
+      'findAccount.findPasswordTab.resetPasswordButtonText';
+  static const findAccount_findPasswordTab_error_sameOldPassword =
+      'findAccount.findPasswordTab.error.sameOldPassword';
+  static const findAccount_findPasswordTab_error_invalidPassword =
+      'findAccount.findPasswordTab.error.invalidPassword';
+  static const findAccount_findPasswordTab_error =
+      'findAccount.findPasswordTab.error';
+  static const findAccount_findPasswordTab_success_title =
+      'findAccount.findPasswordTab.success.title';
+  static const findAccount_findPasswordTab_success_desc =
+      'findAccount.findPasswordTab.success.desc';
+  static const findAccount_findPasswordTab_success_signInButtonText =
+      'findAccount.findPasswordTab.success.signInButtonText';
+  static const findAccount_findPasswordTab_success_resetPasswordButtonText =
+      'findAccount.findPasswordTab.success.resetPasswordButtonText';
+  static const findAccount_findPasswordTab_success =
+      'findAccount.findPasswordTab.success';
+  static const findAccount_findPasswordTab = 'findAccount.findPasswordTab';
+  static const findAccount = 'findAccount';
 }
