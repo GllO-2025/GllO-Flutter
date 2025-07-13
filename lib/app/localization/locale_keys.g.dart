4ead4c85a0583539cd9a3c84e0e31b7abc15660a
generalChang <rhdrnsckdgh0921@naver.com>
2025-07-14 00:57:34 +0900

    feat: 피드 업로드 텍스트 데이터 추가

--- lib/app/localization/locale_keys.g.dart
+++ lib/app/localization/locale_keys.g.dart
@@ -28,109 +28,112 @@ abstract class LocaleKeys {
   static const signIn_error_wrongPassword = 'signIn.error.wrongPassword';
   static const signIn_error = 'signIn.error';
   static const signIn = 'signIn';
-  static const signUpBirthDay_appBarTitle = 'signUpBirthDay.appBarTitle';
-  static const signUpBirthDay_title = 'signUpBirthDay.title';
-  static const signUpBirthDay_desc = 'signUpBirthDay.desc';
-  static const signUpBirthDay_inputHintText = 'signUpBirthDay.inputHintText';
-  static const signUpBirthDay_nextButtonText = 'signUpBirthDay.nextButtonText';
-  static const signUpBirthDay_error_invalidBirthDay =
-      'signUpBirthDay.error.invalidBirthDay';
-  static const signUpBirthDay_error = 'signUpBirthDay.error';
-  static const signUpBirthDay = 'signUpBirthDay';
-  static const signUpTerms_appBarTitle = 'signUpTerms.appBarTitle';
-  static const signUpTerms_title = 'signUpTerms.title';
-  static const signUpTerms_desc = 'signUpTerms.desc';
-  static const signUpTerms_allAgreeText = 'signUpTerms.allAgreeText';
-  static const signUpTerms_termsText = 'signUpTerms.termsText';
-  static const signUpTerms_locationBasedTermsText =
-      'signUpTerms.locationBasedTermsText';
-  static const signUpTerms_marketingConsentText =
-      'signUpTerms.marketingConsentText';
-  static const signUpTerms_label_required = 'signUpTerms.label.required';
-  static const signUpTerms_label_optional = 'signUpTerms.label.optional';
-  static const signUpTerms_label = 'signUpTerms.label';
-  static const signUpTerms_nextButtonText = 'signUpTerms.nextButtonText';
-  static const signUpTerms = 'signUpTerms';
-  static const signUpEmail_appBarTitle = 'signUpEmail.appBarTitle';
-  static const signUpEmail_title = 'signUpEmail.title';
-  static const signUpEmail_desc = 'signUpEmail.desc';
-  static const signUpEmail_emailInputHintText =
-      'signUpEmail.emailInputHintText';
-  static const signUpEmail_verifyStudentIdCard =
-      'signUpEmail.verifyStudentIdCard';
-  static const signUpEmail_codeInputHintText = 'signUpEmail.codeInputHintText';
-  static const signUpEmail_resendCodeText = 'signUpEmail.resendCodeText';
-  static const signUpEmail_sendCodeText = 'signUpEmail.sendCodeText';
-  static const signUpEmail_verifyCodeText = 'signUpEmail.verifyCodeText';
-  static const signUpEmail_emailInputHelperMessage =
-      'signUpEmail.emailInputHelperMessage';
-  static const signUpEmail_nextButtonText = 'signUpEmail.nextButtonText';
-  static const signUpEmail_error_invalidEmail =
-      'signUpEmail.error.invalidEmail';
-  static const signUpEmail_error_wrongCode = 'signUpEmail.error.wrongCode';
-  static const signUpEmail_error = 'signUpEmail.error';
-  static const signUpEmail = 'signUpEmail';
-  static const signUpNameAndPassword_appBarTitle =
-      'signUpNameAndPassword.appBarTitle';
-  static const signUpNameAndPassword_title = 'signUpNameAndPassword.title';
-  static const signUpNameAndPassword_desc = 'signUpNameAndPassword.desc';
-  static const signUpNameAndPassword_nameInputHintText =
-      'signUpNameAndPassword.nameInputHintText';
-  static const signUpNameAndPassword_nameInputHelperMessage =
-      'signUpNameAndPassword.nameInputHelperMessage';
-  static const signUpNameAndPassword_canUseNameHelperMessage =
-      'signUpNameAndPassword.canUseNameHelperMessage';
-  static const signUpNameAndPassword_passwordInputHintText =
-      'signUpNameAndPassword.passwordInputHintText';
-  static const signUpNameAndPassword_passwordInputHelperMessage =
-      'signUpNameAndPassword.passwordInputHelperMessage';
-  static const signUpNameAndPassword_confirmPasswordInputHintText =
-      'signUpNameAndPassword.confirmPasswordInputHintText';
-  static const signUpNameAndPassword_nextButtonText =
-      'signUpNameAndPassword.nextButtonText';
-  static const signUpNameAndPassword_error_duplicateName =
-      'signUpNameAndPassword.error.duplicateName';
-  static const signUpNameAndPassword_error_invalidName =
-      'signUpNameAndPassword.error.invalidName';
-  static const signUpNameAndPassword_error_exceedLimit =
-      'signUpNameAndPassword.error.exceedLimit';
-  static const signUpNameAndPassword_error_mismatchPassword =
-      'signUpNameAndPassword.error.mismatchPassword';
-  static const signUpNameAndPassword_error = 'signUpNameAndPassword.error';
-  static const signUpNameAndPassword = 'signUpNameAndPassword';
-  static const signUpCountry_appBarTitle = 'signUpCountry.appBarTitle';
-  static const signUpCountry_title = 'signUpCountry.title';
-  static const signUpCountry_desc = 'signUpCountry.desc';
-  static const signUpCountry_countryInputHintText =
-      'signUpCountry.countryInputHintText';
-  static const signUpCountry_nextButtonText = 'signUpCountry.nextButtonText';
-  static const signUpCountry = 'signUpCountry';
-  static const signUpSuccess_title = 'signUpSuccess.title';
-  static const signUpSuccess_desc = 'signUpSuccess.desc';
-  static const signUpSuccess_buttonText = 'signUpSuccess.buttonText';
-  static const signUpSuccess = 'signUpSuccess';
-  static const signUpStudentIdCard_appBarTitle =
-      'signUpStudentIdCard.appBarTitle';
-  static const signUpStudentIdCard_title = 'signUpStudentIdCard.title';
-  static const signUpStudentIdCard_desc = 'signUpStudentIdCard.desc';
-  static const signUpStudentIdCard_imageUploadLabel =
-      'signUpStudentIdCard.imageUploadLabel';
-  static const signUpStudentIdCard_imageUploadHintText =
-      'signUpStudentIdCard.imageUploadHintText';
-  static const signUpStudentIdCard_openCameraButtonText =
-      'signUpStudentIdCard.openCameraButtonText';
-  static const signUpStudentIdCard_nextButtonText =
-      'signUpStudentIdCard.nextButtonText';
-  static const signUpStudentIdCard_error_exceedCapacity_title =
-      'signUpStudentIdCard.error.exceedCapacity.title';
-  static const signUpStudentIdCard_error_exceedCapacity_desc =
-      'signUpStudentIdCard.error.exceedCapacity.desc';
-  static const signUpStudentIdCard_error_exceedCapacity_button =
-      'signUpStudentIdCard.error.exceedCapacity.button';
-  static const signUpStudentIdCard_error_exceedCapacity =
-      'signUpStudentIdCard.error.exceedCapacity';
-  static const signUpStudentIdCard_error = 'signUpStudentIdCard.error';
-  static const signUpStudentIdCard = 'signUpStudentIdCard';
+  static const signUp_birthDay_appBarTitle = 'signUp.birthDay.appBarTitle';
+  static const signUp_birthDay_title = 'signUp.birthDay.title';
+  static const signUp_birthDay_desc = 'signUp.birthDay.desc';
+  static const signUp_birthDay_inputHintText = 'signUp.birthDay.inputHintText';
+  static const signUp_birthDay_nextButtonText =
+      'signUp.birthDay.nextButtonText';
+  static const signUp_birthDay_error_invalidBirthDay =
+      'signUp.birthDay.error.invalidBirthDay';
+  static const signUp_birthDay_error = 'signUp.birthDay.error';
+  static const signUp_birthDay = 'signUp.birthDay';
+  static const signUp_terms_appBarTitle = 'signUp.terms.appBarTitle';
+  static const signUp_terms_title = 'signUp.terms.title';
+  static const signUp_terms_desc = 'signUp.terms.desc';
+  static const signUp_terms_allAgreeText = 'signUp.terms.allAgreeText';
+  static const signUp_terms_termsText = 'signUp.terms.termsText';
+  static const signUp_terms_locationBasedTermsText =
+      'signUp.terms.locationBasedTermsText';
+  static const signUp_terms_marketingConsentText =
+      'signUp.terms.marketingConsentText';
+  static const signUp_terms_label_required = 'signUp.terms.label.required';
+  static const signUp_terms_label_optional = 'signUp.terms.label.optional';
+  static const signUp_terms_label = 'signUp.terms.label';
+  static const signUp_terms_nextButtonText = 'signUp.terms.nextButtonText';
+  static const signUp_terms = 'signUp.terms';
+  static const signUp_email_appBarTitle = 'signUp.email.appBarTitle';
+  static const signUp_email_title = 'signUp.email.title';
+  static const signUp_email_desc = 'signUp.email.desc';
+  static const signUp_email_emailInputHintText =
+      'signUp.email.emailInputHintText';
+  static const signUp_email_verifyStudentIdCard =
+      'signUp.email.verifyStudentIdCard';
+  static const signUp_email_codeInputHintText =
+      'signUp.email.codeInputHintText';
+  static const signUp_email_resendCodeText = 'signUp.email.resendCodeText';
+  static const signUp_email_sendCodeText = 'signUp.email.sendCodeText';
+  static const signUp_email_verifyCodeText = 'signUp.email.verifyCodeText';
+  static const signUp_email_emailInputHelperMessage =
+      'signUp.email.emailInputHelperMessage';
+  static const signUp_email_nextButtonText = 'signUp.email.nextButtonText';
+  static const signUp_email_error_invalidEmail =
+      'signUp.email.error.invalidEmail';
+  static const signUp_email_error_wrongCode = 'signUp.email.error.wrongCode';
+  static const signUp_email_error = 'signUp.email.error';
+  static const signUp_email = 'signUp.email';
+  static const signUp_nameAndPassword_appBarTitle =
+      'signUp.nameAndPassword.appBarTitle';
+  static const signUp_nameAndPassword_title = 'signUp.nameAndPassword.title';
+  static const signUp_nameAndPassword_desc = 'signUp.nameAndPassword.desc';
+  static const signUp_nameAndPassword_nameInputHintText =
+      'signUp.nameAndPassword.nameInputHintText';
+  static const signUp_nameAndPassword_nameInputHelperMessage =
+      'signUp.nameAndPassword.nameInputHelperMessage';
+  static const signUp_nameAndPassword_canUseNameHelperMessage =
+      'signUp.nameAndPassword.canUseNameHelperMessage';
+  static const signUp_nameAndPassword_passwordInputHintText =
+      'signUp.nameAndPassword.passwordInputHintText';
+  static const signUp_nameAndPassword_passwordInputHelperMessage =
+      'signUp.nameAndPassword.passwordInputHelperMessage';
+  static const signUp_nameAndPassword_confirmPasswordInputHintText =
+      'signUp.nameAndPassword.confirmPasswordInputHintText';
+  static const signUp_nameAndPassword_nextButtonText =
+      'signUp.nameAndPassword.nextButtonText';
+  static const signUp_nameAndPassword_error_duplicateName =
+      'signUp.nameAndPassword.error.duplicateName';
+  static const signUp_nameAndPassword_error_invalidName =
+      'signUp.nameAndPassword.error.invalidName';
+  static const signUp_nameAndPassword_error_exceedLimit =
+      'signUp.nameAndPassword.error.exceedLimit';
+  static const signUp_nameAndPassword_error_mismatchPassword =
+      'signUp.nameAndPassword.error.mismatchPassword';
+  static const signUp_nameAndPassword_error = 'signUp.nameAndPassword.error';
+  static const signUp_nameAndPassword = 'signUp.nameAndPassword';
+  static const signUp_country_appBarTitle = 'signUp.country.appBarTitle';
+  static const signUp_country_title = 'signUp.country.title';
+  static const signUp_country_desc = 'signUp.country.desc';
+  static const signUp_country_countryInputHintText =
+      'signUp.country.countryInputHintText';
+  static const signUp_country_nextButtonText = 'signUp.country.nextButtonText';
+  static const signUp_country = 'signUp.country';
+  static const signUp_success_title = 'signUp.success.title';
+  static const signUp_success_desc = 'signUp.success.desc';
+  static const signUp_success_buttonText = 'signUp.success.buttonText';
+  static const signUp_success = 'signUp.success';
+  static const signUp_studentIdCard_appBarTitle =
+      'signUp.studentIdCard.appBarTitle';
+  static const signUp_studentIdCard_title = 'signUp.studentIdCard.title';
+  static const signUp_studentIdCard_desc = 'signUp.studentIdCard.desc';
+  static const signUp_studentIdCard_imageUploadLabel =
+      'signUp.studentIdCard.imageUploadLabel';
+  static const signUp_studentIdCard_imageUploadHintText =
+      'signUp.studentIdCard.imageUploadHintText';
+  static const signUp_studentIdCard_openCameraButtonText =
+      'signUp.studentIdCard.openCameraButtonText';
+  static const signUp_studentIdCard_nextButtonText =
+      'signUp.studentIdCard.nextButtonText';
+  static const signUp_studentIdCard_error_exceedCapacity_title =
+      'signUp.studentIdCard.error.exceedCapacity.title';
+  static const signUp_studentIdCard_error_exceedCapacity_desc =
+      'signUp.studentIdCard.error.exceedCapacity.desc';
+  static const signUp_studentIdCard_error_exceedCapacity_button =
+      'signUp.studentIdCard.error.exceedCapacity.button';
+  static const signUp_studentIdCard_error_exceedCapacity =
+      'signUp.studentIdCard.error.exceedCapacity';
+  static const signUp_studentIdCard_error = 'signUp.studentIdCard.error';
+  static const signUp_studentIdCard = 'signUp.studentIdCard';
+  static const signUp = 'signUp';
   static const findAccount_appBarTitle = 'findAccount.appBarTitle';
   static const findAccount_findIdTab_name = 'findAccount.findIdTab.name';
   static const findAccount_findIdTab_idInputHintText =
@@ -186,4 +189,74 @@ abstract class LocaleKeys {
       'findAccount.findPasswordTab.success';
   static const findAccount_findPasswordTab = 'findAccount.findPasswordTab';
   static const findAccount = 'findAccount';
+  static const feedUpload_selectMedia_appBarTitle =
+      'feedUpload.selectMedia.appBarTitle';
+  static const feedUpload_selectMedia_videoTabName =
+      'feedUpload.selectMedia.videoTabName';
+  static const feedUpload_selectMedia_photoTabName =
+      'feedUpload.selectMedia.photoTabName';
+  static const feedUpload_selectMedia_nextButtonText =
+      'feedUpload.selectMedia.nextButtonText';
+  static const feedUpload_selectMedia = 'feedUpload.selectMedia';
+  static const feedUpload_post_appBarTitle = 'feedUpload.post.appBarTitle';
+  static const feedUpload_post_titleInputLabel =
+      'feedUpload.post.titleInputLabel';
+  static const feedUpload_post_titleInputHintText =
+      'feedUpload.post.titleInputHintText';
+  static const feedUpload_post_descInputLabel =
+      'feedUpload.post.descInputLabel';
+  static const feedUpload_post_descInputHintText =
+      'feedUpload.post.descInputHintText';
+  static const feedUpload_post_placeLabel = 'feedUpload.post.placeLabel';
+  static const feedUpload_post_foodCategoryLabel =
+      'feedUpload.post.foodCategoryLabel';
+  static const feedUpload_post_tagLabel = 'feedUpload.post.tagLabel';
+  static const feedUpload_post_uploadButtonText =
+      'feedUpload.post.uploadButtonText';
+  static const feedUpload_post_tag_title = 'feedUpload.post.tag.title';
+  static const feedUpload_post_tag_label = 'feedUpload.post.tag.label';
+  static const feedUpload_post_tag_addTagButtonText =
+      'feedUpload.post.tag.addTagButtonText';
+  static const feedUpload_post_tag_note = 'feedUpload.post.tag.note';
+  static const feedUpload_post_tag_inputHintText =
+      'feedUpload.post.tag.inputHintText';
+  static const feedUpload_post_tag_uploadButtonText =
+      'feedUpload.post.tag.uploadButtonText';
+  static const feedUpload_post_tag = 'feedUpload.post.tag';
+  static const feedUpload_post_foodCategory_title =
+      'feedUpload.post.foodCategory.title';
+  static const feedUpload_post_foodCategory_label =
+      'feedUpload.post.foodCategory.label';
+  static const feedUpload_post_foodCategory_note =
+      'feedUpload.post.foodCategory.note';
+  static const feedUpload_post_foodCategory_korean =
+      'feedUpload.post.foodCategory.korean';
+  static const feedUpload_post_foodCategory_western =
+      'feedUpload.post.foodCategory.western';
+  static const feedUpload_post_foodCategory_japanese =
+      'feedUpload.post.foodCategory.japanese';
+  static const feedUpload_post_foodCategory_chinese =
+      'feedUpload.post.foodCategory.chinese';
+  static const feedUpload_post_foodCategory_cafe =
+      'feedUpload.post.foodCategory.cafe';
+  static const feedUpload_post_foodCategory_unique =
+      'feedUpload.post.foodCategory.unique';
+  static const feedUpload_post_foodCategory_uploadButtonText =
+      'feedUpload.post.foodCategory.uploadButtonText';
+  static const feedUpload_post_foodCategory = 'feedUpload.post.foodCategory';
+  static const feedUpload_post_place_title = 'feedUpload.post.place.title';
+  static const feedUpload_post_place_label = 'feedUpload.post.place.label';
+  static const feedUpload_post_place_note = 'feedUpload.post.place.note';
+  static const feedUpload_post_place_inputHintText =
+      'feedUpload.post.place.inputHintText';
+  static const feedUpload_post_place_uploadOnePlaceButtonText =
+      'feedUpload.post.place.uploadOnePlaceButtonText';
+  static const feedUpload_post_place_uploadAtLeastTwoPlaceButtonText =
+      'feedUpload.post.place.uploadAtLeastTwoPlaceButtonText';
+  static const feedUpload_post_place = 'feedUpload.post.place';
+  static const feedUpload_post_success_toastMessage =
+      'feedUpload.post.success.toastMessage';
+  static const feedUpload_post_success = 'feedUpload.post.success';
+  static const feedUpload_post = 'feedUpload.post';
+  static const feedUpload = 'feedUpload';
 }
