b9c1542e0a43e9dd1e9d164f82da45d48253d007
generalChang <rhdrnsckdgh0921@naver.com>
2025-07-21 22:12:46 +0900

    fix: 에러 객체 매핑 로직 수정

--- lib/core/error_handling/error_model.dart
+++ lib/core/error_handling/error_model.dart
@@ -5,12 +5,23 @@ part 'generated/error_model.g.dart';
 /// Http Error Response 객체
 @JsonSerializable()
 class ErrorModel {
-  factory ErrorModel.fromJson(Map<String, dynamic> json) =>
-      _$ErrorModelFromJson(json);
-  ErrorModel({
-    required this.status,
-    this.errorMessage = const ErrorModelMessage(),
-  });
+  factory ErrorModel.fromJson(json) {
+    if (json == null || json is! Map<String, dynamic>) {
+      return ErrorModel();
+    }
+
+    return ErrorModel(
+      status: json['status'] as String,
+      errorMessage:
+          json['errorMessage'] == null
+              ? const ErrorModelMessage()
+              : ErrorModelMessage.fromJson(
+                json['errorMessage'] as Map<String, dynamic>,
+              ),
+    );
+  }
+
+  ErrorModel({this.status = '', this.errorMessage = const ErrorModelMessage()});
 
   final String status;
   final ErrorModelMessage errorMessage;
